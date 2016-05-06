2357343148f13882061d44a9fdec43b50193d30c
Brandon Sherette <brandonsherette@gmail.com>
2016-05-05 17:32:31 -0700

    Fixed jquery dependency version number and updated plugin version to v0.0.4

--- dist/inline-toggle-menu.js
+++ dist/inline-toggle-menu.js
@@ -1,7 +1,7 @@
 /**
  * angular-inline-toggle-menu - Angular version of the 'inline-toggle-menu'.
  * @authors Brandon Sherette
- * @version v0.0.3
+ * @version v0.0.4
  * @link 
  * @license MIT
  */
