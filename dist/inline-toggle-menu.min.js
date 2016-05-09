809be25857f963f9721f3fa4d6dc89015a0d3e3b
Brandon Sherette <brandonsherette@gmail.com>
2016-05-08 20:39:49 -0700

    Updated to v0.1.1

--- dist/inline-toggle-menu.min.js
+++ dist/inline-toggle-menu.min.js
@@ -1,7 +1,7 @@
 /**
  * angular-inline-toggle-menu - Angular version of the 'inline-toggle-menu'.
  * @authors Brandon Sherette
- * @version v0.1.0
+ * @version v0.1.1
  * @link 
  * @license MIT
  */
